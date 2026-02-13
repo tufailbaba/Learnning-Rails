3616a8963c0aeb90be16577b4b0e110aa2267b34
Tufail <tufail@example.com>
2026-02-13 06:10:27 +0000

    Saving Student into DB using form

--- Student-mgmt/app/controllers/students_controller.rb
+++ Student-mgmt/app/controllers/students_controller.rb
@@ -6,4 +6,17 @@ class StudentsController < ApplicationController
   def new
     @student = Student.new
   end
+  def create
+    @student = Student.new(student_params)
+    if @student.save
+     redirect_to students_path
+
+    else
+      render :new
+    end
+  end
+  private
+    def student_params
+      params.require(:student).permit(:first_name, :last_name, :email)
+    end
 end
