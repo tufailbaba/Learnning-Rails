38f0475d7bdfcfb806e09fc7fe86058665582a60
Tufail <tufail@example.com>
2026-02-16 05:39:21 +0000

    Editing and updating students

--- Student-mgmt/app/controllers/students_controller.rb
+++ Student-mgmt/app/controllers/students_controller.rb
@@ -6,15 +6,31 @@ class StudentsController < ApplicationController
   def new
     @student = Student.new
   end
-  def create
-    @student = Student.new(student_params)
-    if @student.save
-     redirect_to students_path
+ def create
+  @student = Student.new(student_params)
 
+  if @student.save
+    redirect_to students_path
+  else
+    render :new, status: :unprocessable_entity
+  end
+end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+  def edit
+    @student = Student.find(params[:id])
+  end
+  def update
+    @student = Student.find(params[:id])
+    if @student.update(student_params)
+      redirect_to @student
     else
-      render :new
+      render :edit, status: :unprocessable_entity
     end
   end
+
   private
     def student_params
       params.require(:student).permit(:first_name, :last_name, :email)
