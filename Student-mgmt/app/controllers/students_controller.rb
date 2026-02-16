70b701216b42030716d46b95838c872721545e5d
Tufail <tufail@example.com>
2026-02-16 06:06:27 +0000

    Optimized Student Controller code

--- Student-mgmt/app/controllers/students_controller.rb
+++ Student-mgmt/app/controllers/students_controller.rb
@@ -1,4 +1,5 @@
 class StudentsController < ApplicationController
+  before_action :set_student, only: %i[ show edit update destroy ]
   def index
     @students = Student.all
   end
@@ -17,13 +18,10 @@ class StudentsController < ApplicationController
 end
 
   def show
-    @student = Student.find(params[:id])
   end
   def edit
-    @student = Student.find(params[:id])
   end
   def update
-    @student = Student.find(params[:id])
     if @student.update(student_params)
       redirect_to @student
     else
@@ -31,7 +29,6 @@ end
     end
   end
   def destroy
-    @student = Student.find(params[:id])
     @student.destroy
     redirect_to students_path
   end
@@ -39,5 +36,8 @@ end
   private
     def student_params
       params.require(:student).permit(:first_name, :last_name, :email)
+    end
+    def set_student
+      @student = Student.find(params[:id])
     end
 end
