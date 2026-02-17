b61f3f5ee26c9f21e61d93d07ef5685b43b3bab2
Tufail <tufail@example.com>
2026-02-17 05:33:45 +0000

    Migration for Creating Tables

--- Student-mgmt/db/schema.rb
+++ Student-mgmt/db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_02_17_042745) do
+ActiveRecord::Schema[8.0].define(version: 2026_02_17_052949) do
   create_table "courses", force: :cascade do |t|
     t.string "name"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "demos", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
