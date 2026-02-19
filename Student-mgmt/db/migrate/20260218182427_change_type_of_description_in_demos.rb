df5d14612120096d4bed39ecf03b29aa601b512b
Tufail <tufail@example.com>
2026-02-19 06:45:45 +0000

    Some Changes with migration

--- Student-mgmt/db/migrate/20260218182427_change_type_of_description_in_demos.rb
+++
@@ -1,8 +0,0 @@
-class ChangeTypeOfDescriptionInDemos < ActiveRecord::Migration[8.0]
-  def up
-    change_column :demos, :description, :text
-  end
-  def down
-    change_column :demos, :description, :string
-  end
-end
--- Student-mgmt/db/schema.rb
+++ Student-mgmt/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_02_18_182427) do
+ActiveRecord::Schema[8.0].define(version: 2026_02_17_063619) do
   create_table "courses", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -20,7 +20,7 @@ ActiveRecord::Schema[8.0].define(version: 2026_02_18_182427) do
 
   create_table "demos", force: :cascade do |t|
     t.string "name"
-    t.text "description"
+    t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
