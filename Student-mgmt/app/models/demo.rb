40fe24b93dc0afa45394882ccad2fb13e86af7c6
Tufail <tufail@example.com>
2026-02-20 06:10:28 +0000

    Active record callbacks

--- Student-mgmt/app/models/demo.rb
+++ Student-mgmt/app/models/demo.rb
@@ -1,3 +1,8 @@
 class Demo < ApplicationRecord
     has_one :sub_demo
+
+    before_create :greeting_message
+    def greeting_message
+        puts "Hey i will run before creating the record"
+    end
 end
--- Student-mgmt/app/models/student.rb
+++ Student-mgmt/app/models/student.rb
@@ -4,5 +4,14 @@ class Student < ApplicationRecord
     has_many :student_projects
     has_many :projects, through: :student_projects
     validates :first_name, :last_name, :email, presence: true
+    after_create :display_student_age
+    def display_student_age
+        if self.date_of_birth.present?
+            age = ((Time.zone.now - self.date_of_birth.to_time) / 1.year.seconds).floor
+            puts "The age of the student is #{age} years."
+        else
+            puts "Date of birth is not provided."
+        end
+    end
 end
                         
