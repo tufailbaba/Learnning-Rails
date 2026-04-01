f71087738d370efff36d1713b52bc16817c63ec8
Tufail <tufail@example.com>
2026-04-01 05:33:22 +0000

    final fix folders

---
+++ college_voice/config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  # Restrict routes to only allow viewing and creating
+  resources :complaints, only: [:index, :show, :new, :create]
+  
+  # Make the complaints list the homepage
+  root "complaints#index"
+end
